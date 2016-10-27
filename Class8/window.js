4f6e64586551d44b521045fa7f15e256da6b0ffc
Jeremy Ott <ott.jeremych@gmail.com>
2016-10-27 18:26:08 -0400

    adding these new files to git

---
+++ Class8/window.js
@@ -0,0 +1,19 @@
+var window = {
+  document: {
+    head: {
+      title: "Mike's Page"
+    },
+    body: [
+      {
+      element: "h1",
+      body: "Hello World"
+    },
+    {
+      element: "h2",
+      body: "Goodnight Moon"
+    }
+    ]
+  },
+  localStorage: {}
+
+}
